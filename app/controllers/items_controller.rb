e828032520f348cda43d22416a4194641254a355
takechi-single <t.takaaki0906@gmail.com>
2020-11-18 19:24:33 +0900

    Active_Hashのモデル作成・データ入力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,23 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+    Item.save
+  end
+
   def show
     
   end
 
   def edit
     
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :text, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one :order
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_date
 end
